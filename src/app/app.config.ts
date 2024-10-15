253758fef36934267edc4293fbe7e865cbb4c0fa
Pietro_Albano <piealba@gmail.com>
2024-10-15 18:20:17 +0200

    angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { appRoutes } from './app.routes';  // Import corretto
 
+// Configurazione corretta del routing
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),  // Utilizza 'appRoutes' invece di 'routes'
+    provideClientHydration()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GenericComponent } from './generic/generic.component';
+import { ElementsComponent } from './elements/elements.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: GenericComponent },       // Homepage
+  { path: 'generic', component: GenericComponent },
+  { path: 'elements', component: ElementsComponent }
+];
